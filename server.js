7d89613c5f26e7599c1681b7440e11c7511cd9cb
Thanakorn Chonmanus <thanakornchonmanus@Thanakorns-MacBook-Pro.local>
2026-03-03 20:21:27 +0700

    User Login

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
+const cookieParser = require('cookie-parser');
 
 const hotels = require("./routes/hotels");
+const users = require("./routes/auth");
 
 // Load env vars
 dotenv.config({ path: './config/config.env' });
@@ -16,13 +18,21 @@ const app = express();
 app.set("query parser", "extended");
 // Body parser
 app.use(express.json());
+// Cookie parser
+app.use(cookieParser());
 
 // Route files
 app.use("/api/v1/hotels", hotels);
+app.use("/api/v1/auth", users);
 
 
 const PORT = process.env.PORT || 5050;
-app.listen(PORT, () => {
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
+const server = app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+
+// Handle unhandled promise rejections
+process.on("unhandledRejection", (err, promise) => {
+    console.log(`Error: ${err.message}`);
+    // Close server & exit process
+    server.close(() => process.exit(1));
 });
 
